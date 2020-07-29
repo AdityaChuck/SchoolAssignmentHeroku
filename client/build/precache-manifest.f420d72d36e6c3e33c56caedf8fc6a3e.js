e4ffe15fcd457e4185dfcefb53bbb0cf290c42f8
BATMAN <tadim777@gmail.com>
2020-07-29 12:47:02 +0530

    hosted with heroku

---
+++ client/build/precache-manifest.f420d72d36e6c3e33c56caedf8fc6a3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a73cf9649e1ab2639bff967a87b1d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5db999d6879cf8a46a6d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8182912745db2f226b62",
+    "url": "/static/js/2.767e09b2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.767e09b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5db999d6879cf8a46a6d",
+    "url": "/static/js/main.102a3a9d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
